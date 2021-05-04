b8914fe9f19e17d3f8f43a7a8bccf246effa7da5
Kioshi Okamoto <kjor29@gmail.com>
2021-05-04 13:48:24 -0500

    :dart: IMPROVE: testimonial section y footer

--- src/sections/team-section.js
+++ src/sections/team-section.js
@@ -13,186 +13,196 @@ import Member5 from 'assets/team/member-5.png';
 import Member6 from 'assets/team/member-6.png';
 
 const data = [
-  {
-    id: 1,
-    imgSrc: Member1,
-    altText: 'Saimon Harmer',
-    title: 'Saimon Harmer',
-    designation: 'CEO and Founder',
-    socialProfile: [
-      {
-        id: 1,
-        name: 'facebook',
-        path: '#',
-        icon: <FaFacebookF />,
-      },
-      {
-        id: 2,
-        name: 'twitter',
-        path: '#',
-        icon: <FaTwitter />,
-      },
-      {
-        id: 3,
-        name: 'instagram',
-        path: '#',
-        icon: <FaInstagram />,
-      },
-    ],
-  },
-  {
-    id: 2,
-    imgSrc: Member2,
-    altText: 'Aaron Nunez',
-    title: 'Aaron Nunez',
-    designation: 'Founder',
-    socialProfile: [
-      {
-        id: 1,
-        name: 'facebook',
-        path: '#',
-        icon: <FaFacebookF />,
-      },
-      {
-        id: 2,
-        name: 'twitter',
-        path: '#',
-        icon: <FaTwitter />,
-      },
-      {
-        id: 3,
-        name: 'instagram',
-        path: '#',
-        icon: <FaInstagram />,
-      },
-    ],
-  },
-  {
-    id: 3,
-    imgSrc: Member3,
-    altText: 'Aaron Nunez',
-    title: 'Aaron Nunez',
-    designation: 'Web Designer',
-    socialProfile: [
-      {
-        id: 1,
-        name: 'facebook',
-        path: '#',
-        icon: <FaFacebookF />,
-      },
-      {
-        id: 2,
-        name: 'twitter',
-        path: '#',
-        icon: <FaTwitter />,
-      },
-      {
-        id: 3,
-        name: 'instagram',
-        path: '#',
-        icon: <FaInstagram />,
-      },
-    ],
-  },
-  {
-    id: 4,
-    imgSrc: Member4,
-    altText: 'Lina Jutila',
-    title: 'Lina Jutila',
-    designation: 'Web Developer',
-    socialProfile: [
-      {
-        id: 1,
-        name: 'facebook',
-        path: '#',
-        icon: <FaFacebookF />,
-      },
-      {
-        id: 2,
-        name: 'twitter',
-        path: '#',
-        icon: <FaTwitter />,
-      },
-      {
-        id: 3,
-        name: 'instagram',
-        path: '#',
-        icon: <FaInstagram />,
-      },
-    ],
-  },
-  {
-    id: 5,
-    imgSrc: Member5,
-    altText: 'Saimon Harmer',
-    title: 'Saimon Harmer',
-    designation: 'CEO and Founder',
-    socialProfile: [
-      {
-        id: 1,
-        name: 'facebook',
-        path: '#',
-        icon: <FaFacebookF />,
-      },
-      {
-        id: 2,
-        name: 'twitter',
-        path: '#',
-        icon: <FaTwitter />,
-      },
-      {
-        id: 3,
-        name: 'instagram',
-        path: '#',
-        icon: <FaInstagram />,
-      },
-    ],
-  },
-  {
-    id: 6,
-    imgSrc: Member6,
-    altText: 'Aaron Nunez',
-    title: 'Aaron Nunez',
-    designation: 'Web Designer',
-    socialProfile: [
-      {
-        id: 1,
-        name: 'facebook',
-        path: '#',
-        icon: <FaFacebookF />,
-      },
-      {
-        id: 2,
-        name: 'twitter',
-        path: '#',
-        icon: <FaTwitter />,
-      },
-      {
-        id: 3,
-        name: 'instagram',
-        path: '#',
-        icon: <FaInstagram />,
-      },
-    ],
-  },
+	{
+		id: 1,
+		imgSrc: Member1,
+		altText: 'Saimon Harmer',
+		title: 'Saimon Harmer',
+		designation: 'CEO and Founder',
+		socialProfile: [
+			{
+				id: 1,
+				name: 'facebook',
+				path: '#',
+				icon: <FaFacebookF />,
+			},
+			{
+				id: 2,
+				name: 'twitter',
+				path: '#',
+				icon: <FaTwitter />,
+			},
+			{
+				id: 3,
+				name: 'instagram',
+				path: '#',
+				icon: <FaInstagram />,
+			},
+		],
+	},
+	{
+		id: 2,
+		imgSrc: Member2,
+		altText: 'Aaron Nunez',
+		title: 'Aaron Nunez',
+		designation: 'Founder',
+		socialProfile: [
+			{
+				id: 1,
+				name: 'facebook',
+				path: '#',
+				icon: <FaFacebookF />,
+			},
+			{
+				id: 2,
+				name: 'twitter',
+				path: '#',
+				icon: <FaTwitter />,
+			},
+			{
+				id: 3,
+				name: 'instagram',
+				path: '#',
+				icon: <FaInstagram />,
+			},
+		],
+	},
+	{
+		id: 3,
+		imgSrc: Member3,
+		altText: 'Aaron Nunez',
+		title: 'Aaron Nunez',
+		designation: 'Web Designer',
+		socialProfile: [
+			{
+				id: 1,
+				name: 'facebook',
+				path: '#',
+				icon: <FaFacebookF />,
+			},
+			{
+				id: 2,
+				name: 'twitter',
+				path: '#',
+				icon: <FaTwitter />,
+			},
+			{
+				id: 3,
+				name: 'instagram',
+				path: '#',
+				icon: <FaInstagram />,
+			},
+		],
+	},
+	{
+		id: 4,
+		imgSrc: Member4,
+		altText: 'Lina Jutila',
+		title: 'Lina Jutila',
+		designation: 'Web Developer',
+		socialProfile: [
+			{
+				id: 1,
+				name: 'facebook',
+				path: '#',
+				icon: <FaFacebookF />,
+			},
+			{
+				id: 2,
+				name: 'twitter',
+				path: '#',
+				icon: <FaTwitter />,
+			},
+			{
+				id: 3,
+				name: 'instagram',
+				path: '#',
+				icon: <FaInstagram />,
+			},
+		],
+	},
+	{
+		id: 5,
+		imgSrc: Member5,
+		altText: 'Saimon Harmer',
+		title: 'Saimon Harmer',
+		designation: 'CEO and Founder',
+		socialProfile: [
+			{
+				id: 1,
+				name: 'facebook',
+				path: '#',
+				icon: <FaFacebookF />,
+			},
+			{
+				id: 2,
+				name: 'twitter',
+				path: '#',
+				icon: <FaTwitter />,
+			},
+			{
+				id: 3,
+				name: 'instagram',
+				path: '#',
+				icon: <FaInstagram />,
+			},
+		],
+	},
+	{
+		id: 6,
+		imgSrc: Member6,
+		altText: 'Aaron Nunez',
+		title: 'Aaron Nunez',
+		designation: 'Web Designer',
+		socialProfile: [
+			{
+				id: 1,
+				name: 'facebook',
+				path: '#',
+				icon: <FaFacebookF />,
+			},
+			{
+				id: 2,
+				name: 'twitter',
+				path: '#',
+				icon: <FaTwitter />,
+			},
+			{
+				id: 3,
+				name: 'instagram',
+				path: '#',
+				icon: <FaInstagram />,
+			},
+		],
+	},
 ];
 
 export default function TeamSection() {
-  return (
-    <h1>Team Section</h1>
-  );
+	return (
+		<section>
+			<Container>
+				<SectionHeader slogan="Our Team" title="The most qualified and talented individuals" />
+				<Grid sx={styles.grid}>
+					{data.map((item) => (
+						<TeamCard
+							key={item.id}
+							src={item.imgSrc}
+							alt={item.altText}
+							title={item.title}
+							designation={item.designation}
+							social={item.socialProfile}
+						/>
+					))}
+				</Grid>
+			</Container>
+		</section>
+	);
 }
 
 const styles = {
-  grid: {
-    mt: [0, null, -6, null, -4],
-    gridGap: ['35px 0px', null, 0, null, null, '30px 35px'],
-    gridTemplateColumns: [
-      'repeat(2,1fr)',
-      null,
-      'repeat(2,1fr)',
-      null,
-      'repeat(3,1fr)',
-    ],
-  },
+	grid: {
+		mt: [0, null, -6, null, -4],
+		gridGap: ['35px 0px', null, 0, null, null, '30px 35px'],
+		gridTemplateColumns: ['repeat(2,1fr)', null, 'repeat(2,1fr)', null, 'repeat(3,1fr)'],
+	},
 };
